79bdd200055c630cc304da1574a3336da484143f
Rick South <rick@natamus.com>
2025-02-20 13:49:37 +0100

    Enchantment Level Cap Indicator source code for 1.20.1.

---
+++ Common/src/main/java/com/natamus/enchantmentlevelcapindicator/util/Util.java
@@ -0,0 +1,54 @@
+package com.natamus.enchantmentlevelcapindicator.util;
+
+import com.mojang.datafixers.util.Pair;
+import com.natamus.collective.functions.NumberFunctions;
+import com.natamus.enchantmentlevelcapindicator.config.ConfigHandler;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+import javax.annotation.Nullable;
+
+public class Util {
+	public static @Nullable Pair<Enchantment, Integer> parseRawComponentText(Component component, Registry<Enchantment> enchantmentRegistry) {
+		return parseRawComponentText(component.toString(), enchantmentRegistry);
+	}
+	public static @Nullable Pair<Enchantment, Integer> parseRawComponentText(String rawText, Registry<Enchantment> enchantmentRegistry) {
+		String[] rawEnchSpl = rawText.split("enchantment.");
+        if (rawEnchSpl.length < 2) {
+            return null;
+        }
+
+        String rawEnchantment = rawEnchSpl[1].split("'")[0];
+		ResourceLocation enchantmentResourceLocation = new ResourceLocation(rawEnchantment.replace(".", ":"));
+
+		int level = 1;
+		if (rawEnchSpl.length > 2) {
+			String rawEnchantmentLevel = rawEnchSpl[2].split("'")[0].replace("level.", "");
+
+			if (!NumberFunctions.isNumeric(rawEnchantmentLevel)) {
+				return null;
+			}
+
+			level = Integer.parseInt(rawEnchantmentLevel);
+		}
+
+        Enchantment enchantment = enchantmentRegistry.get(enchantmentResourceLocation);
+        if (enchantment == null) {
+            return null;
+        }
+
+		return Pair.of(enchantment, level);
+	}
+
+	public static Component getStarComponent() {
+		ChatFormatting symbolColour = ChatFormatting.getById(ConfigHandler.maxLevelSymbolColourIndex);
+		if (symbolColour == null) {
+			symbolColour = ChatFormatting.GOLD;
+		}
+
+		return Component.literal(" " + ConfigHandler.maxLevelSymbol).withStyle(symbolColour);
+	}
+}
