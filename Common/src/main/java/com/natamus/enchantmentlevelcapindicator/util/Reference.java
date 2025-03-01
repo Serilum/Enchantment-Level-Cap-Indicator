3bcd1c992f9924f7246b75f9a023f4dc86b1bee6
Rick South <rick@natamus.com>
2025-03-01 10:26:51 +0100

    (v1.1) Fixed a mod incompatibility with Enchantment Descriptions.

--- Common/src/main/java/com/natamus/enchantmentlevelcapindicator/util/Reference.java
+++ Common/src/main/java/com/natamus/enchantmentlevelcapindicator/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.enchantmentlevelcapindicator.util;
 public class Reference {
 	public static final String MOD_ID = "enchantmentlevelcapindicator";
 	public static final String NAME = "Enchantment Level Cap Indicator";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
