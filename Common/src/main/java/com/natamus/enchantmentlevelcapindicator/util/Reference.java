f6d2a0a887ddc1c962778cbb853f859f24467071
Rick South <rick@natamus.com>
2025-10-08 01:40:05 +0200

    Enchantment Level Cap Indicator source code for 1.21.10.

---
+++ Common/src/main/java/com/natamus/enchantmentlevelcapindicator/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.enchantmentlevelcapindicator.util;
+
+public class Reference {
+	public static final String MOD_ID = "enchantmentlevelcapindicator";
+	public static final String NAME = "Enchantment Level Cap Indicator";
+	public static final String VERSION = "1.2";
+	public static final String ACCEPTED_VERSIONS = "[1.21.10]";
+}
